f8256e5f84212c2184946dfa1e5337c6006b4eeb
zbwadud <zbwadud@gmail.com>
2015-04-27 12:41:11 +1200

    Update

--- src/main/java/nzqa/camel/test/acamedemo1/MyRouteBuilder.java
+++ src/main/java/nzqa/camel/test/acamedemo1/MyRouteBuilder.java
@@ -38,7 +38,7 @@ public class MyRouteBuilder extends RouteBuilder {
         System.out.println("testing build job....");
         System.out.println("testing build job....");
         System.out.println("testing build job....");
-        
+        System.out.println("testing build job....");
         Main.main(args);
     }
 
